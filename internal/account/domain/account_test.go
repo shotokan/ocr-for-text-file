8472eea3237c87f822a957ff2c4ebc8b78f62284
iisabido <ivan.sabido@encora.com>
2022-02-03 10:43:40 -0600

    add success tests when account is created

--- internal/account/domain/account_test.go
+++ internal/account/domain/account_test.go
@@ -4,7 +4,31 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestAccountSuccess(t *testing.T) {
+	accountID := uuid.NewString()
+	accountNumber := "000000001"
+	accountNumberIll := "??0000001"
+	tests := []struct{
+		name string
+		input []string
+		output *Account
+	}{
+		{name: "create account", input: []string{accountID, accountNumber},  output: &Account{ id: accountID, number: accountNumber }},
+		{name: "create account with illegible numbers", input: []string{accountID, accountNumberIll},  output: &Account{ id: accountID, number: accountNumberIll }},
+
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewAccount(tt.input[0], tt.input[1])
+			assert.Nil(t, err, tt.name)
+			assert.EqualValues(t, tt.output, got, tt.name)
+		})
+	}
+}
 
 func TestAccountFail(t *testing.T) {
 	accountID := uuid.NewString()
@@ -20,10 +44,8 @@ func TestAccountFail(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewAccount(tt.input[0], tt.input[1])
-			if err.Error() != tt.output {
-				t.Errorf("NewAccount() = %v, want %v", got, tt.output)
-			}
+			_, err := NewAccount(tt.input[0], tt.input[1])
+			assert.Equal(t, tt.output, err.Error(), tt.name)
 		})
 	}
 }
